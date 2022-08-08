c83c4187f46e9c5278f832644ab72982b39d4243
Joni Kivimäki <joni.kivimaki@elisanet.fi>
2022-08-08 19:26:43 +0300

    Päivitystä käyttikseen ja JS

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 const idTunnus = document.querySelector("#kayttajaID").value;
+const sukupuolet = document.querySelectorAll("input[name='sukupuoli']");
+const kielet = document.querySelectorAll("input[name='kieli']");
+
+let valittuSukupuoli;
+for (const sukupuoliValinta of sukupuolet) {
+    if (sukupuoliValinta.checked) {
+        valittuSukupuoli = sukupuoliValinta.value;
+        break;
+    }
+}
+
+let valittuKieli;
+for (const kieliValinta of kielet) {
+    if (kieliValinta.checked) {
+        valittuKieli = kieliValinta.value;
+        break;
+    }
+}
