5712b9f881e8839caba80a7846d077e7ad273382
Joni Kivimäki <joni.kivimaki@elisanet.fi>
2022-08-15 10:37:02 +0300

    Siirto

--- main.js
+++ main.js
@@ -1,33 +1,62 @@
 const idTunnus = document.querySelector("#kayttajaID").value;
 const kielet = document.querySelectorAll("input[name='kieli']");
 
-document.getElementById("kayttajaID").addEventListener("invalid", idSisaltaa);
-document.getElementById("send").addEventListener("click", radioValittu);
-document.getElementById("send").addEventListener("click", kieliValittu);
+document.getElementById("send").addEventListener("click", kaynnista);
+// document.getElementById("send").addEventListener("click", radioValittu);
+// document.getElementById("send").addEventListener("click", kieliValittu);
 
-function idSisaltaa() {
-    console.log("Täytyy sisältää vähintään 6 merkkiä");
-    alert ("ID:n täytyy sisältää vähintään 6 merkkiä");
-}
-
-function radioValittu() {
+function kaynnista(event) {
     event.preventDefault();
+
+    // Pakollinen ID 6 merkkiä
+    let idLength = idTunnus.length;
+    if (idLength <6) {
+        console.log("Täytyy sisältää vähintään 6 merkkiä");
+        alert ("ID:n täytyy sisältää vähintään 6 merkkiä");
+    }
+
+
+    // Sukupuolen pakollinen valinta
     var mies = document.getElementById("sukupuoliMies").checked;
     var nainen = document.getElementById("sukupuoliNainen").checked;
     var muu = document.getElementById("sukupuoliMuu").checked;
 
     if (!mies && !nainen && !muu) {
+        console.log("Valitse sukupuoli");
         alert("Valitse sukupuoli");
     }
- }
 
- function kieliValittu() {
-    event.preventDefault();
+    //Kielen pakollinen valinta 
     var suomi = document.getElementById("kieliSuomi").checked;
     var svenska = document.getElementById("kieliSve").checked;
     var englanti = document.getElementById("kieliEng").checked;
 
     if (!suomi && !svenska && !englanti) {
+        console.log("Valitse kieli");
         alert("Valitse kieli");
     }
- }
+}
+
+// function radioValittu() {
+//     Event.preventDefault();
+//     var mies = document.getElementById("sukupuoliMies").checked;
+//     var nainen = document.getElementById("sukupuoliNainen").checked;
+//     var muu = document.getElementById("sukupuoliMuu").checked;
+
+//     if (!mies && !nainen && !muu) {
+//         console.log("Valitse sukupuoli");
+//         alert("Valitse sukupuoli");
+//     }
+//  }
+
+//  function kieliValittu() {
+//     event.preventDefault();
+//     var suomi = document.getElementById("kieliSuomi").checked;
+//     var svenska = document.getElementById("kieliSve").checked;
+//     var englanti = document.getElementById("kieliEng").checked;
+
+//     if (!suomi && !svenska && !englanti) {
+//         console.log("Valitse kieli");
+//         alert("Valitse kieli");
+//     }
+//  }
